2db3d27828a1d50fa459f5c9d7369d511125c90c
hebasaadosman <hebaosman443@gmail.com>
2022-08-02 17:37:11 +0300

    start angularapp

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,10 +33,12 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+           
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+             services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +55,7 @@ namespace API
 
             app.UseRouting();
 
+app.UseCors(policy=>policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));//Middle ware
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
